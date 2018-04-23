5d5d6610d10a66962c84352cadfbbd87c410302e
Stephanie Youstra <steph.you07@gmail.com>
2018-04-23 10:39:50 -0400

    Challenge: Move toast to the top of the screen

--- 1_GeoQuiz/app/src/main/java/com/example/android/geoquiz/QuizActivity.java
+++ 1_GeoQuiz/app/src/main/java/com/example/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.example.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -20,9 +21,12 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
+
+                Toast toast = Toast.makeText(QuizActivity.this,
                         R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0,250);
+                toast.show();
             }
         });
 
@@ -30,9 +34,11 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
+                Toast toast = Toast.makeText(QuizActivity.this,
                         R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,0,250);
+                toast.show();
             }
         });
     }
